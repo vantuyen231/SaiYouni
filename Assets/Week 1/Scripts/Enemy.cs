c154b504788536c95f8218ae73d8a09d2ddbc331
vantuyen231 <dovantuyen21112002@gmail.com>
2025-03-25 22:56:15 +0700

    firt commit

--- Assets/Week 1/Scripts/Enemy.cs
+++ Assets/Week 1/Scripts/Enemy.cs
@@ -8,13 +8,13 @@ public class Enemy : MonoBehaviour
     // 5 Enemies variables
 
     //variables 1 
-
+    [SerializeField] private float maxHp;
     //variables 2
-
+    [SerializeField] private float currentHp;
     //variables 3 
-
+    [SerializeField] private int damegeEnemy;
     //variables 4 
-
+    [SerializeField] private float speed;
     //variables 5 
 
 
--- Assets/Week 1/Scripts/MaxValues.cs
+++ Assets/Week 1/Scripts/MaxValues.cs
@@ -8,14 +8,17 @@ public class MaxValues : MonoBehaviour
     {
         // Giá trị lớn nhất của int
         int maxInt = 0;
+        maxInt = int.MaxValue;
         Debug.Log("Giá trị lớn nhất của int: " + maxInt);
 
         // Giá trị lớn nhất của float
         float maxFloat = 0;
+        maxFloat = float.MaxValue;
         Debug.Log("Giá trị lớn nhất của float: " + maxFloat);
 
         // Giá trị lớn nhất của double
         double maxDouble = 0;
+        maxDouble = double.MaxValue;
         Debug.Log("Giá trị lớn nhất của double: " + maxDouble);
     }
 
